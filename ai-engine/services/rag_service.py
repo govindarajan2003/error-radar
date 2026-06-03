b3a8c72816e8207fddb7e970b830bbc83e2841b7
Govindarajan D <govindarajan14092003@gmail.com>
2026-06-03 17:30:59 +0530

    Runs llm: returns result in specified format.

--- ai-engine/services/rag_service.py
+++ ai-engine/services/rag_service.py
@@ -1,7 +1,11 @@
 import os
+import requests
+import json
 from dotenv import load_dotenv
 from sqlalchemy import text, create_engine
 from services.embedding_service import get_embedding
+from pathlib import Path
+
 
 # Loads environment values
 load_dotenv()
@@ -75,4 +79,67 @@ def find_similar_errors(
                 }
                 results_list.append(match)
     return results_list
+
+def generate_suggestion(
+        query_text: str,
+        similar_errors: list[dict],
+):
+    model = os.getenv('LLM_MODEL')
+    api_url = os.getenv('OLLAMA_BASE_URL') + "/api/generate"
+    try:
+        project_root = Path(__file__).resolve().parent.parent
+        suggestion_prompt_path = project_root / "prompts" / "suggestion.txt"
+        with open(suggestion_prompt_path,"r") as suggestion:
+            content = suggestion.read()
+    except FileNotFoundError as e:
+        return {"root_cause": "Prompt missing", "suggested_fix": "Check file path", "confidence": 0.0}
+
+    
+    prompt = content.format(
+        query_error=query_text,
+        historical_errors= json.dumps(similar_errors, indent=2)
+    )
+
+    payload = {
+        'model':model,
+        'prompt':prompt,
+        'stream':False,
+        'format':'json',
+        'options':{
+            "temperature": 0
+        }
+    }
+    try:
+        response = requests.post(api_url, json=payload)
+        response.raise_for_status()
+        response_data = response.json()
+        raw_text = response_data.get('response','')
+    except  requests.exceptions.Timeout as err:
+        return {"root_cause": "Connection Timeout", "suggested_fix": "Try again Later", "confidence": 0.0}
+    except requests.exceptions.ConnectionError:
+        return {"root_cause": "Connection Issue", "suggested_fix": "Check if ollama engine is running", "confidence": 0.0}
+    
+    clean_text = raw_text.strip()
+    if clean_text.startswith('```json'):
+        clean_text = clean_text[7:]
+    elif clean_text.startswith('```'):
+        clean_text = clean_text[3:]
+    if clean_text.endswith('```'):
+        clean_text = clean_text[:-3]
+    clean_text = clean_text.strip()
+    
+    try:
+        parsed_json = json.loads(clean_text)
+        
+        formatted_json = { 
+            "root_cause": parsed_json.get("root_cause"),
+            "suggested_fix": parsed_json.get("suggested_fix"),
+            "confidence": parsed_json.get("confidence")} 
+        return formatted_json
+    
+    except json.JSONDecodeError as e:
+        return {"root_cause": "LLM failed to return valid JSON", "suggested_fix": "Manual review required", "confidence": 0.0}
+        
+        
+    
 
